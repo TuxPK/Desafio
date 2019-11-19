2280351dacf2ffbc4bcf904261ae7b48be66e943
Marcos Rogerio Gauderth Junior <40186386+TuxPK@users.noreply.github.com>
2019-11-19 15:01:49 -0300

    Correção rota

--- src/routes.js
+++ src/routes.js
@@ -13,16 +13,16 @@ routes.put('/promotions', PromotionController.update);
 routes.delete('/promotions/:id', PromotionController.delete);
 
 routes.get('/awards', AwardController.index);
-routes.get('/awards/:name', AwardController.show);
+routes.get('/awards/:title', AwardController.show);
 routes.post('/awards', AwardController.store);
 routes.put('/awards', AwardController.update);
 routes.delete('/awards/:id', AwardController.delete);
 
-routes.get('/promotionawards', PromotionAwardController.index);
-routes.get('/promotionawards/:name', PromotionAwardController.show);
-routes.post('/promotionawards', PromotionAwardController.store);
-routes.put('/promotionawards', PromotionAwardController.update);
-routes.delete('/promotionawards/:id', PromotionAwardController.delete);
+routes.get('/promotionAwards', PromotionAwardController.index);
+routes.get('/promotionAwards/:idPromotion/:idAward', PromotionAwardController.show);
+routes.post('/promotionAwards', PromotionAwardController.store);
+routes.put('/promotionAwards', PromotionAwardController.update);
+routes.delete('/promotionAwards/:id', PromotionAwardController.delete);
 
 export default routes;
 

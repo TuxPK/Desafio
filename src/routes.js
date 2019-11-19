9f94ad89ece41e4d9492f663843c41b84830a0eb
Marcos Rogerio Gauderth Junior <40186386+TuxPK@users.noreply.github.com>
2019-11-19 14:47:18 -0300

    Rotas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
 import PromotionController from './app/controllers/PromotionController';
+import AwardController from './app/controllers/AwardController';
+import PromotionAwardController from './app/controllers/PromotionAwardController';
 
 const routes = new Router();
 
@@ -9,6 +11,18 @@ routes.get('/promotions/:name', PromotionController.show);
 routes.post('/promotions', PromotionController.store);
 routes.put('/promotions', PromotionController.update);
 routes.delete('/promotions/:id', PromotionController.delete);
+
+routes.get('/awards', AwardController.index);
+routes.get('/awards/:name', AwardController.show);
+routes.post('/awards', AwardController.store);
+routes.put('/awards', AwardController.update);
+routes.delete('/awards/:id', AwardController.delete);
+
+routes.get('/promotionawards', PromotionAwardController.index);
+routes.get('/promotionawards/:name', PromotionAwardController.show);
+routes.post('/promotionawards', PromotionAwardController.store);
+routes.put('/promotionawards', PromotionAwardController.update);
+routes.delete('/promotionawards/:id', PromotionAwardController.delete);
 
 export default routes;
 
